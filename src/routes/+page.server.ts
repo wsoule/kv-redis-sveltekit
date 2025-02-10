3db1f686689397471070c56f5b0b0ab5a860750d
Wyat Soule <wsoule679@gmail.com>
2025-02-10 11:33:07 -0500

    init

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,17 @@
+import { KV_URL } from '$env/static/private'
+import { createClient } from 'redis'
+
+const kv = await createClient({
+  url: KV_URL,
+}).connect()
+
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+  const pageVisits = await kv.get('pageVisits')
+  await kv.set('pageVisits', Number.parseInt(pageVisits ?? '0', 10) + 1)
+  const updatedPageVisits = await kv.get('pageVisits')
+
+  return {
+    pageVisits: updatedPageVisits,
+  }
+}
